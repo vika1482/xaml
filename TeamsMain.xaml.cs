32539109a9e50e04e4deca591fb7084bc1cdc426
Vika Atikeeva <vika13377@mail.ru>
2021-12-13 22:57:37 +0700

    xaml

---
+++ TeamsMain.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XamlApp
+{
+	/// <summary>
+	/// Логика взаимодействия для TeamsMain.xaml
+	/// </summary>
+	public partial class TeamsMain : Window
+	{
+		public TeamsMain()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			VisitorMenu visitorMenu = new VisitorMenu();
+			visitorMenu.Show();
+			this.Close();
+		}
+	}
+}
---
+++ VisitorMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XamlApp
+{
+	/// <summary>
+	/// Логика взаимодействия для VisitorMenu.xaml
+	/// </summary>
+	public partial class VisitorMenu : Window
+	{
+		public VisitorMenu()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			TeamsMain teamsMain = new TeamsMain();
+			teamsMain.Show();
+			this.Close();
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			MainWindow mainWindow = new MainWindow();
+			mainWindow.Show();
+			this.Close();
+		}
+	}
+}
